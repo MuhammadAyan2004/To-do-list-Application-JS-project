b5f13e8d840c79f75c65b8323e38c1cb203a004f
Muhammad AYAN <m101ayan92@gmail.com>
2024-09-12 17:02:06 +0500

    update

--- script.js
+++ script.js
@@ -1,58 +1,50 @@
 const input = document.getElementById("inputBox");
 const addBtn = document.getElementById("addBtn");
 const todo = document.getElementById("todoList");
-
-let editTodo=null;
-
-const addTodo=()=>{
+let editTodo = null;
+const addTodo = ()=>{
     const inputText = input.value.trim();
-    if(inputText.length<=0){
-        alert("you must right something");
+    if(input.value === ""){
+        alert("please write something ");
         return false;
-        
     }else if(addBtn.value==="Edit"){
-    editTodo.target.previousElementSibling.innerHTML = inputText;
-    addBtn.value="Add";
-    input.value="";  
-
+        editTodo.target.previousElementSibling.innerHTML=inputText;
+        addBtn.value="Add";
+        input.value="";
     }else{
-        // creating p tag
-    const li = document.createElement("li");
-    const p = document.createElement("p");
-    p.innerHTML=inputText;
-    li.appendChild(p);
-
-    // creating edit button
-    const edit = document.createElement("button");
-    edit.innerText="Edit";
-    edit.classList.add("editBtn");
-    li.appendChild(edit);  
-    // creating delete button
-    const del = document.createElement("button");
-    del.innerText="remove";
-    del.classList.add("delBtn");
-    li.appendChild(del);  
-
-
-    todo.appendChild(li);
-    input.value="";
- }
+        //create p tag
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        p.innerHTML=inputText;
+        li.appendChild(p);
+        todo.appendChild(li);
+        input.value="";
+        // create edit button
+        const edit = document.createElement("button")
+        edit.innerText="Edit";
+        edit.classList.add("editBtn")
+        li.appendChild(edit);
+        // create delete button
+        const del = document.createElement("button")
+        del.innerText="Remove";
+        del.classList.add("delBtn")
+        li.appendChild(del);
+    }
 };
-
 const updateTodo = (e)=>{
-
-    if( e.target.innerHTML ==="remove"){
-    
+    if(e.target.innerHTML==="Remove"){
         todo.removeChild(e.target.parentElement);
-    }else if(e.target.innerHTML ==="Edit"){
-        
-        input.value = e.target.previousElementSibling.innerHTML;
+    }else if (e.target.innerHTML==="Edit"){
+        input.value=e.target.previousElementSibling.innerHTML;
         input.focus();
         addBtn.value="Edit";
         editTodo=e;
-
     }
 }
-
 addBtn.addEventListener("click",addTodo);
-todo.addEventListener("click", updateTodo);
+input.addEventListener("keydown", function(event) {
+    if (event.key === 'Enter') {
+        addTodo();
+    }
+});
+todo.addEventListener("click",updateTodo);
